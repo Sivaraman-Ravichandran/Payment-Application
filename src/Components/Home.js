34ad93879a141a1cff715d27d716df0f3003f40c
Sivaraman-Ravichandran <sivcloud.12@gmail.com>
2024-07-07 01:05:26 +0530

    test commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,18 @@
 import { useParams } from "react-router-dom";
-import { React, useEffect   } from "react";
+import { React, useEffect } from "react";
+import Slit from "./Slider";
 function Home({ getDetails }) {
   const { id } = useParams();
   useEffect(() => {
     getDetails(id);
   }, [id]);
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="slit">
+        <Slit />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
